c9c7784afd7314883ab9a0f9cd606779279d84a6
nir bar levav <nir.barlevav@cycode.com>
2025-09-12 23:11:47 +0300

    deploy frontend

---
+++ frontend/config.template.js
@@ -0,0 +1,7 @@
+// In frontend/config.template.js
+window.APP_CONFIG = {
+    UserPoolId: '__USER_POOL_ID__',
+    ClientId: '__CLIENT_ID__',
+    ApiUrl: '__API_URL__',
+    Region: '__REGION__'
+};
